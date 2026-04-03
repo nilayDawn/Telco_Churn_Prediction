45011cd143628b828633a29470ab902094bad1ba
Nilay Dawn <nilaydawn@gmail.com>
2026-04-03 10:29:15 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+from typing import List
+
+HYPEN_E_DOT = '-e .'    
+def get_requirements(file_path: str) -> List[str]:
+    '''
+    This function will return the list of requirements
+     mentioned in the requirements.txt file
+    '''
+    requirements = []
+    with open(file_path, 'r') as file:
+        requirements = file.readlines()
+        requirements = [req.replace('\n', '') for req in requirements]
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+    return requirements
+
+setup(
+    name='mlproject',
+    version='0.0.1',
+    packages=find_packages(),
+    install_requires=get_requirements("requirements.txt"),
+    author='Nilay Dawn',
+    author_email='nilaydawn@gmail.com'
+)
