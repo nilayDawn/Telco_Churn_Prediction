b00ada6451ba1f59c77d79272514dc3c3a1f62c0
Nilay Dawn <nilaydawn@gmail.com>
2026-04-03 11:06:22 +0530

    exception and logging

---
+++ src/exception.py
@@ -0,0 +1,28 @@
+import sys
+import types
+
+
+def error_message_detail(error,error_detail:types.ModuleType):
+    _,_,exc_tb=error_detail.exc_info()
+    if exc_tb is None:
+        return "Error occured in python script name [{0}] line number [{1}] error message[{2}]".format(
+            "unknown","unknown",str(error))
+    file_name=exc_tb.tb_frame.f_code.co_filename
+    error_message="Error occured in python script name [{0}] line number [{1}] error message[{2}]".format(
+    file_name,exc_tb.tb_lineno,str(error))
+
+    return error_message
+
+    
+
+class CustomException(Exception):
+    def __init__(self,error_message,error_detail:types.ModuleType):
+        super().__init__(error_message)
+        self.error_message=error_message_detail(error_message,error_detail=error_detail)
+    
+    def __str__(self):
+        return self.error_message
+    
+
+
+        
